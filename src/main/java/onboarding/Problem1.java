0e4e12d935c07f59e8bf9bdbd51e10bf60b2f695
JHyun <wogustkfkd11@gmail.com>
2022-11-28 16:04:44 +0900

    fix problem1 commit

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,103 +8,116 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
+
+    static List<Integer> pobi = new ArrayList<>();
+    static List<Integer> crong = new ArrayList<>();
+
+    public static boolean bookPageError(){
+        if(pobi.get(0) + 1 == pobi.get(1) && crong.get(0) + 1 == crong.get(1)) {
+            return true;
+        }
+        return false;
+    }
+    public static int getMaxValue(List<Integer> username){
+        List<Integer> name = new ArrayList<>();
+        name = username;
         int i,j;
         int temp;
         List<Integer> left =  new ArrayList<>();
         List<Integer> right =  new ArrayList<>();
-        int answer = Integer.MAX_VALUE;
-        int left_sum = 0;
-        int right_sum = 0;
-        int left_multi = 1;
-        int right_multi = 1;
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftMulti = 1;
+        int rightMulti = 1;
 
-        int pobi_max = 0;
-        int crong_max = 0;
-        if(pobi.get(0) + 1 == pobi.get(1) && crong.get(0) + 1 == crong.get(1)){
-            temp = pobi.get(0);
-            while(temp > 0) {
-                left.add(temp % 10);
-                temp/= 10;
-            }
-            temp = pobi.get(1);
-            while(temp > 0) {
-                right.add(temp % 10);
-                temp/= 10;
-            }
-            for(i =0; i<left.size(); i++){
-                left_sum += left.get(i);
-                left_multi *= left.get(i);
-            }
-            for(i = 0; i<right.size(); i++){
-                right_sum += right.get(i);
-                right_multi *= right.get(i);
-            }
-            pobi_max = Math.max(Math.max(Math.max(left_sum, right_sum), left_multi), right_multi);
-            left_sum = 0;
-            right_sum = 0;
-            left_multi =1;
-            right_multi = 1;
-            List<Integer> lef =  new ArrayList<>();
-            List<Integer> rig =  new ArrayList<>();
-            temp = crong.get(0);
-            while(temp > 0) {
-                lef.add(temp % 10);
-                temp/= 10;
-            }
-            temp = crong.get(1);
-            while(temp > 0) {
-                rig.add(temp % 10);
-                temp/= 10;
-            }
-            for(i =0; i<lef.size(); i++){
-                left_sum += lef.get(i);
-                left_multi *= lef.get(i);
-            }
-            for(i = 0; i<rig.size(); i++){
-                right_sum += rig.get(i);
-                right_multi *= rig.get(i);
-            }
-            crong_max = Math.max(Math.max(Math.max(left_sum, right_sum), left_multi), right_multi);
-            System.out.println(pobi_max);
-            System.out.println(crong_max);
-
-            if(pobi_max > crong_max)
-                answer = 1;
-            else if(pobi_max < crong_max)
-                answer = 2;
-            else
-                answer = 0;
+        temp = name.get(0);
+        while(temp > 0) {
+            left.add(temp % 10);
+            temp/= 10;
         }
-        else
-            return -1;
 
+        temp = name.get(1);
+        while(temp > 0) {
+            right.add(temp % 10);
+            temp/= 10;
+        }
+
+        for(i =0; i<left.size(); i++){
+            leftSum += left.get(i);
+            leftMulti *= left.get(i);
+        }
+        for(i = 0; i<right.size(); i++){
+            rightSum += right.get(i);
+            rightMulti *= right.get(i);
+        }
+
+        return Math.max(Math.max(Math.max(leftSum, rightSum), leftMulti), rightMulti);
+    }
+    public static int calc(int pobiMaxValue, int crongMaxValue){
+        int answer = -1;
+        System.out.println(pobiMaxValue);
+        System.out.println(crongMaxValue);
+
+        if(pobiMaxValue > crongMaxValue)
+            answer = 1;
+        else if(pobiMaxValue < crongMaxValue)
+            answer = 2;
+        else if(pobiMaxValue == crongMaxValue)
+            answer = 0;
         return answer;
     }
 
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if(bookPageError() == false) {
+            System.out.println("ERROR: 왼쪽, 오른쪽 페이지가 다릅니다.");
+            return  -1;
+        }
+
+        int pobiMaxValue = getMaxValue(pobi);
+        int crongMaxValue = getMaxValue(crong);
+        int calc = calc(pobiMaxValue, crongMaxValue);
+        return calc;
+    }
+
     public static void main(String[] args) throws IOException {
+        int input = input();
+        if(input == -1){
+            System.out.println("ERROR: 숫자를 입력하세요");
+            return;
+        }
+        int solution = solution(pobi, crong);
+        if(solution == -1){
+            return;
+        }
+        System.out.println("winner: " + solution); // pobi-win:1, crong-win:2, draw:0, exception:-1
+
+
+    }
+    public static int input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        List<Integer> pobi = new ArrayList<>();
-        List<Integer> crong = new ArrayList<>();
         System.out.print("page of pobi: ");
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i<2;i++){
-            int n = Integer.parseInt(st.nextToken());
-            pobi.add(n);
+            try{
+                int n = Integer.parseInt(st.nextToken());
+                pobi.add(n);
+            }catch (IllegalArgumentException e){
+                return -1;
+            }
         }
-        for(int i = 0; i<2; i++){
-            System.out.println(pobi.get(i));
-        }
+
         System.out.print("page of crong: ");
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i<2;i++){
-            int n = Integer.parseInt(st.nextToken());
-            crong.add(n);
+            try{
+                int n = Integer.parseInt(st.nextToken());
+                crong.add(n);
+            }catch (IllegalArgumentException e){
+                return -1;
+            }
         }
-
-        System.out.println("winner: " + solution(pobi, crong)); // pobi-win:1, crong-win:2, draw:0, exception:-1
-
         br.close();
+        return 0;
     }
 }
