20221ec1b56495b84039d64b8e7680c7724f3e7b
JAEHYUN <wogustkfkd11@gmail.com>
2022-11-30 20:56:43 +0900

    no overwrite

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,19 +2,25 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        int answer = 0;
+        answer = helper(number);
+        System.out.println(answer);
+
+        return answer;
+    }
+    static int helper(int number){
         int i;
         int answer = 0;
         for(i=1;i<= number;i++){
-            if(i/10 == 3 || i/10 == 6 || i/10 == 9)
+            if(i/10 == 3 || i/10 == 6 || i/10 == 9) // 30, 31,32,33 ..., 60, 61,62,63
                 answer++;
-            if(i%10 == 3|| i%10 == 6 || i%10 == 9)
+            if(i%10 == 3|| i%10 == 6 || i%10 == 9) //3, 6, 9,...,33..
                 answer++;
         }
-        System.out.println(answer);
         return answer;
     }
 
     public static void main(String[] args) {
-        int answer = solution(43);
+        int answer = solution(33);
     }
 }
