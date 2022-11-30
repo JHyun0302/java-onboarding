0df2d2c3dad70727b1b88ecd9c42e697fce8f39c
JAEHYUN <wogustkfkd11@gmail.com>
2022-11-30 20:56:16 +0900

    fixed code commit

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,53 +2,49 @@ package onboarding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        answer = "";
-        int i =0, j=0;
-        char[] crypto = cryptogram.toCharArray();
-        char first;
-        char second;
-        String ans = "";
-        String temp = cryptogram;
-        int cnt=0;
-
-        while(true){
-            cnt =0;
-            for (j = 0; j < crypto.length-1; j++) {
-                if(crypto[j] == crypto[j+1])
-                    cnt++;
-            }
-            if(cnt == 0)
-                break;
-            for (i = 0; i < crypto.length- 1; i++) {
-                first = crypto[i];
-                second = crypto[i + 1];
-
-                if (first != second) {
-                    ans += String.valueOf(first);
-                }
-                else{
-                    i += 1;
-                }
-            }
-            if(ans == "")
-                break;
-            if(crypto[i] >= (char) 97 && crypto[i] <= (char) 122)
-                ans += String.valueOf(crypto[i]);
-
-            crypto = new char[ans.length()];
-            crypto = ans.toCharArray();
-            temp = ans;
-            ans = "";
+        List<String> crypto = new ArrayList<>();
+        int i;
+        boolean check = false;
+        String answer = "";
+        for (i = 0; i < cryptogram.length(); i++) {
+            crypto.add(String.valueOf(cryptogram.charAt(i)));
         }
-        if(temp.charAt(0) == temp.charAt(1))
-            temp = "";
 
-        answer = temp;
+        while (true) {
+            check = false;
+            for (i = 0; i < crypto.size() - 1; i++) {
+                if (crypto.get(i).equals(crypto.get(i + 1))) {
+                    check = true;
+                }
+                if(check == true){
+                    helper(crypto);
+                }
+            }
+            if(check == false){
+                break;
+            }
+        }
+
+        for (String s : crypto) {
+            answer += s;
+        }
         return answer;
+    }
+
+
+    public static void helper(List<String> crypto){
+        int i;
+        for (i =0; i< crypto.size()-1; i++){
+            if(crypto.get(i).equals(crypto.get(i+1))){
+                crypto.remove(i);
+                crypto.remove(i);
+                i--;
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -57,4 +53,5 @@ public class Problem2 {
         answer = solution(cryptogram);
         System.out.println("Solution: " +answer);
     }
+
 }
